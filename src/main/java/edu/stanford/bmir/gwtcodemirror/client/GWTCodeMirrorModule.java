a7d855c03c1235dc4eb93a8a1552744a31112ca2
Matthew Horridge <matthew.horridge@stanford.edu>
2014-03-31 15:31:22 -0700

    Cleaned up imports in module file.

--- src/main/java/edu/stanford/bmir/gwtcodemirror/client/GWTCodeMirrorModule.java
+++ src/main/java/edu/stanford/bmir/gwtcodemirror/client/GWTCodeMirrorModule.java
@@ -1,10 +1,6 @@
 package edu.stanford.bmir.gwtcodemirror.client;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
