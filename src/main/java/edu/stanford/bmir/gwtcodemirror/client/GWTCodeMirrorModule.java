308ef86890337e7c4f954037cd8ba6b1463090c6
Matthew Horridge <matthew.horridge@stanford.edu>
2014-03-31 15:30:19 -0700

    Removed commented out code

--- src/main/java/edu/stanford/bmir/gwtcodemirror/client/GWTCodeMirrorModule.java
+++ src/main/java/edu/stanford/bmir/gwtcodemirror/client/GWTCodeMirrorModule.java
@@ -12,36 +12,5 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class GWTCodeMirrorModule implements EntryPoint {
 
   public void onModuleLoad() {
-//      final GWTCodeMirror w = new GWTCodeMirror();
-//      w.setEnabled(true);
-//      w.addValueChangeHandler(new ValueChangeHandler<String>() {
-//          public void onValueChange(ValueChangeEvent<String> event) {
-//              GWT.log(w.getCaretPosition().toString());
-//          }
-//      });
-//      RootPanel.get("slot").add(w);
-//      w.setValue("Class: X");
-//
-//      w.setValue("Class: Aircraft\n" +
-//              "\n" +
-//              "Annotations:\n" +
-//              "\n" +
-//              "Annotations:\n" +
-//              "xref \"Stuff\"\n" +
-//              "            \n" +
-//              "rdfs:comment \"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum\n" +
-//              "\n" +
-//              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum sequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum\n" +
-//              "\n" +
-//              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum\"\n" +
-//              "\n" +
-//              "SubClassOf:\n" +
-//              "hasPart some (A or B)\n" +
-//              "        \n" +
-//              "EquivalentTo:\n" +
-//              "hasPart some {a, b, c}\n" +
-//              "        \n" +
-//              "DisjointWith:\n" +
-//              "A, B, C, D, E, F");
   }
 }
